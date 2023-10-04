8cc1257ad6c768d277fe5323e7056302fa67571f
Adelia <adeliasalsabila1001@gmail.com>
2023-10-04 19:28:07 +0700

    final script

--- js/script.js
+++ js/script.js
@@ -1,6 +1,15 @@
+// INPUT RESI
 function cekResi(event) {
     const input = document.querySelector("#resi");
     const value = input.value;
     console.log(value);
+
+}
+
+// BAR MENU
+function hendleClick(event){
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide-menu");
+    menu.classList.toggle("show-menu");
 
 }
