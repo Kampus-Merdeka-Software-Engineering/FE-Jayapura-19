6a0b3f54af52971156f597631695fdc2ee5f86ee
Adelia <adeliasalsabila1001@gmail.com>
2023-10-05 17:50:45 +0700

    add

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ function hendleClick(event){
     menu.classList.toggle("show-menu");
 
 }
+// SUBMIT KOMENTAR
+function submitComment(event) {
+    event.preventDefault();
+
+    const opinionTextarea = document.getElementById("opinion-textarea");
+    const commentsList = document.getElementById("comments-list");
+
+    const opinion = opinionTextarea.value;
+    if (opinion.trim() !== "") {
+        const commentItem = document.createElement("li");
+        commentItem.textContent = opinion;
+        commentsList.appendChild(commentItem);
+        opinionTextarea.value = "";
+
+        // Tambahkan komentar ke bagian bawah form
+        const commentSection = document.getElementById("comment-section");
+        commentSection.appendChild(commentItem);
+    }
+}
